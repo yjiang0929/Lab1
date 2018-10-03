7d3a636f4a173428f90d5d573e10ade578f39c6e
yjiang0929 <yichenjiang0929@gmail.com>
2018-10-03 19:37:48 -0400

    testbench with comments

--- alu.t.v
+++ alu.t.v
@@ -86,168 +86,168 @@ initial begin
       $display("R Error");
 
   $display("32 Bit XOR tests");
-  $display("Control |                A                 |                B                 |                R                   |COut | OFL |ZERO |Cout Exp| OFL Exp|ZERP Exp");
-  control=3'd2; a = 32'h87654321; b = 32'h12345678; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  $display("Control |                A                 |                B                 |                R                   ");
+  control=3'd2; a = 32'h87654321; b = 32'h12345678; #`DELAY //XOR two normal cases
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a^b)
       $display("R Error");
-  control=3'd2; a = 32'hFFFFFFFF; b = 32'h12345678; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  control=3'd2; a = 32'hFFFFFFFF; b = 32'h12345678; #`DELAY //XOR with all 1
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a^b)
       $display("R Error");
-  control=3'd2; a = 32'hFFFFFFFF; b = 32'hFFFFFFFF; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   1", control, a, b, r, co, ofl, zero);
+  control=3'd2; a = 32'hFFFFFFFF; b = 32'hFFFFFFFF; #`DELAY //XOR two all 1 cases
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a^b)
       $display("R Error");
-  control=3'd2; a = 32'h00000000; b = 32'h12345678; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  control=3'd2; a = 32'h00000000; b = 32'h12345678; #`DELAY //XOR with all 0
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a^b)
       $display("R Error");
-  control=3'd2; a = 32'h00000000; b = 32'h00000000; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   1", control, a, b, r, co, ofl, zero);
+  control=3'd2; a = 32'h00000000; b = 32'h00000000; #`DELAY //XOR two all 0 cases
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a^b)
       $display("R Error");
-  control=3'd2; a = 32'hFFFFFFFF; b = 32'h00000000; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  control=3'd2; a = 32'hFFFFFFFF; b = 32'h00000000; #`DELAY //XOR all 0 and all 1
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a^b)
       $display("R Error");
 
   $display("32 Bit SLT tests");
-  $display("Control |                A                 |                B                 |                R                   |COut | OFL |ZERO |Cout Exp| OFL Exp|ZERP Exp");
+  $display("Control |                A                 |                B                 |                R                   ");
   control=3'd3; a = 32'h00000000; b = 32'h00000000; #`DELAY //0=0
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   1", control, a, b, r, co, ofl, zero);
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != 32'h00000000)
       $display("R Error");
   control=3'd3; a = 32'h7FFFFFFF; b = 32'h12345678; #`DELAY //pos>pos
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   1", control, a, b, r, co, ofl, zero);
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != 32'h00000000)
       $display("R Error");
   control=3'd3; a = 32'h12345678; b = 32'h7FFFFFFF; #`DELAY //pos<pos
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != 32'h00000001)
       $display("R Error");
   control=3'd3; a = 32'hFFFFFFFF; b = 32'h81234567; #`DELAY //neg>neg
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   1", control, a, b, r, co, ofl, zero);
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != 32'h00000000)
       $display("R Error");
   control=3'd3; a = 32'h81234567; b = 32'hFFFFFFFF; #`DELAY //neg<neg
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != 32'h00000001)
       $display("R Error");
   control=3'd3; a = 32'h7FFFFFFF; b = 32'hFFFFFFFF; #`DELAY //pos>neg
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   1", control, a, b, r, co, ofl, zero);
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != 32'h00000000)
       $display("R Error");
   control=3'd3; a = 32'hFFFFFFFF; b = 32'h7FFFFFFF; #`DELAY //neg<pos
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != 32'h00000001)
       $display("R Error");
 
   $display("32 Bit AND tests");
-  $display("Control |                A                 |                B                 |                R                   |COut | OFL |ZERO |Cout Exp| OFL Exp|ZERP Exp");
-  control=3'd4; a = 32'h87654321; b = 32'h12345678; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  $display("Control |                A                 |                B                 |                R                   ");
+  control=3'd4; a = 32'h87654321; b = 32'h12345678; #`DELAY //AND two normal cases
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a&b)
       $display("R Error");
-  control=3'd4; a = 32'hFFFFFFFF; b = 32'h12345678; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  control=3'd4; a = 32'hFFFFFFFF; b = 32'h12345678; #`DELAY //AND with all 1
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a&b)
       $display("R Error");
-  control=3'd4; a = 32'hFFFFFFFF; b = 32'hFFFFFFFF; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  control=3'd4; a = 32'hFFFFFFFF; b = 32'hFFFFFFFF; #`DELAY //AND two all 1 cases
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a&b)
       $display("R Error");
-  control=3'd4; a = 32'h00000000; b = 32'h12345678; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   1", control, a, b, r, co, ofl, zero);
+  control=3'd4; a = 32'h00000000; b = 32'h12345678; #`DELAY //AND with all 0
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a&b)
       $display("R Error");
-  control=3'd4; a = 32'h00000000; b = 32'h00000000; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   1", control, a, b, r, co, ofl, zero);
+  control=3'd4; a = 32'h00000000; b = 32'h00000000; #`DELAY //AND two all 0 cases
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a&b)
       $display("R Error");
-  control=3'd4; a = 32'hFFFFFFFF; b = 32'h00000000; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   1", control, a, b, r, co, ofl, zero);
+  control=3'd4; a = 32'hFFFFFFFF; b = 32'h00000000; #`DELAY //AND all 0 and all 1
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a&b)
       $display("R Error");
 
   $display("32 Bit NAND tests");
   $display("Control |                A                 |                B                 |                R                   |COut | OFL |ZERO |Cout Exp| OFL Exp|ZERP Exp");
-  control=3'd5; a = 32'h87654321; b = 32'h12345678; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  control=3'd5; a = 32'h87654321; b = 32'h12345678; #`DELAY //NAND two normal cases
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a~&b)
       $display("R Error");
-  control=3'd5; a = 32'hFFFFFFFF; b = 32'h12345678; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  control=3'd5; a = 32'hFFFFFFFF; b = 32'h12345678; #`DELAY //NAND with all 1
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a~&b)
       $display("R Error");
-  control=3'd5; a = 32'hFFFFFFFF; b = 32'hFFFFFFFF; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   1", control, a, b, r, co, ofl, zero);
+  control=3'd5; a = 32'hFFFFFFFF; b = 32'hFFFFFFFF; #`DELAY //NAND two all 1 cases
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a~&b)
       $display("R Error");
-  control=3'd5; a = 32'h00000000; b = 32'h12345678; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  control=3'd5; a = 32'h00000000; b = 32'h12345678; #`DELAY //NAND with all 0
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a~&b)
       $display("R Error");
-  control=3'd5; a = 32'h00000000; b = 32'h00000000; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  control=3'd5; a = 32'h00000000; b = 32'h00000000; #`DELAY //NAND two all 0 cases
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a~&b)
       $display("R Error");
-  control=3'd5; a = 32'hFFFFFFFF; b = 32'h00000000; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  control=3'd5; a = 32'hFFFFFFFF; b = 32'h00000000; #`DELAY //NAND all 0 and all 1
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a~&b)
       $display("R Error");
 
   $display("32 Bit NOR tests");
-  $display("Control |                A                 |                B                 |                R                   |COut | OFL |ZERO |Cout Exp| OFL Exp|ZERP Exp");
-  control=3'd6; a = 32'h87654321; b = 32'h12345678; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  $display("Control |                A                 |                B                 |                R                   ");
+  control=3'd6; a = 32'h87654321; b = 32'h12345678; #`DELAY //NOR two normal cases
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a~|b)
       $display("R Error");
-  control=3'd6; a = 32'hFFFFFFFF; b = 32'h12345678; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   1", control, a, b, r, co, ofl, zero);
+  control=3'd6; a = 32'hFFFFFFFF; b = 32'h12345678; #`DELAY //NOR with all 1
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a~|b)
       $display("R Error");
-  control=3'd6; a = 32'hFFFFFFFF; b = 32'hFFFFFFFF; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   1", control, a, b, r, co, ofl, zero);
+  control=3'd6; a = 32'hFFFFFFFF; b = 32'hFFFFFFFF; #`DELAY //NOR two all 1 cases
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a~|b)
       $display("R Error");
-  control=3'd6; a = 32'h00000000; b = 32'h12345678; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  control=3'd6; a = 32'h00000000; b = 32'h12345678; #`DELAY //NOR with all 0
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a~|b)
       $display("R Error");
-  control=3'd6; a = 32'h00000000; b = 32'h00000000; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  control=3'd6; a = 32'h00000000; b = 32'h00000000; #`DELAY //NOR two all 0 cases
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a~|b)
       $display("R Error");
-  control=3'd6; a = 32'hFFFFFFFF; b = 32'h00000000; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   1", control, a, b, r, co, ofl, zero);
+  control=3'd6; a = 32'hFFFFFFFF; b = 32'h00000000; #`DELAY //NOR all 1 and all 0 cases
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a~|b)
       $display("R Error");
 
   $display("32 Bit OR tests");
-  $display("Control |                A                 |                B                 |                R                   |COut | OFL |ZERO |Cout Exp| OFL Exp|ZERP Exp");
-  control=3'd7; a = 32'h87654321; b = 32'h12345678; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  $display("Control |                A                 |                B                 |                R                   ");
+  control=3'd7; a = 32'h87654321; b = 32'h12345678; #`DELAY //OR two normal cases
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a|b)
       $display("R Error");
-  control=3'd7; a = 32'hFFFFFFFF; b = 32'h12345678; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  control=3'd7; a = 32'hFFFFFFFF; b = 32'h12345678; #`DELAY //OR with all 1 cases
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a|b)
       $display("R Error");
-  control=3'd7; a = 32'hFFFFFFFF; b = 32'hFFFFFFFF; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  control=3'd7; a = 32'hFFFFFFFF; b = 32'hFFFFFFFF; #`DELAY //OR two all 1 cases
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a|b)
       $display("R Error");
-  control=3'd7; a = 32'h00000000; b = 32'h12345678; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  control=3'd7; a = 32'h00000000; b = 32'h12345678; #`DELAY //OR with all 0 cases
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a|b)
       $display("R Error");
-  control=3'd7; a = 32'h00000000; b = 32'h00000000; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   1", control, a, b, r, co, ofl, zero);
+  control=3'd7; a = 32'h00000000; b = 32'h00000000; #`DELAY //OR two all 0 cases
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a|b)
       $display("R Error");
-  control=3'd7; a = 32'hFFFFFFFF; b = 32'h00000000; #`DELAY
-  $display("%b  | %b | %b |  %b  |  %b  |  %b  |  %b  |   0    |   0    |   0", control, a, b, r, co, ofl, zero);
+  control=3'd7; a = 32'hFFFFFFFF; b = 32'h00000000; #`DELAY //OR all 1 and all 0 cases
+  $display("%b  | %b | %b |  %b  ", control, a, b, r);
   if (r != a|b)
       $display("R Error");
 end
